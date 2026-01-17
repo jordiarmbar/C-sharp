df667d06f1b97880e3d9dfc47b5d5dc17c4f23d4
Jordi A <jordiarmbar@campus.monlau.com>
2026-01-17 18:34:20 +0100

    Add files via upload

---
+++ RA2_Voz_WinForms/WinMicro/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WinMicro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(75, 333);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(173, 53);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Iniciar voz";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(484, 333);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(164, 53);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Parar voz";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(352, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "---";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(174, 105);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(416, 142);
+            this.panel1.TabIndex = 3;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(46, 253);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(706, 22);
+            this.txtLog.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtLog;
+    }
+}
+
